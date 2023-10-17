84e1b43a4ce309f68efeec8b2ab501085609b638
tuhinhossain123 <csetuhin55@gmail.com>
2023-10-17 21:05:00 +0600

    projects setup

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware 
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res)=>{
+    res.send('Brand Shop Is Running')
+})
+app.listen(port, ()=>{
+    console.log('My Port running On:', port)
+})
